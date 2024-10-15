18016e8d79e7945e0ef3bb32ce5acba44929db8a
far1h <farihmuhammad45@gmail.com>
2024-10-15 09:09:27 +0700

    Add two local query scopes

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -17,4 +17,12 @@ class Book extends Model
     public function scopeTitle(Builder $query, string $title) {
         return $query->where('title','LIKE','%'.$title.'%');
     }
+
+    public function scopePopular(Builder $query) {
+        return $query->withCount('reviews')->orderBy('reviews_count','desc');
+    }
+
+    public function scopeHighestRated(Builder $query) {
+        return $query->withAvg('reviews','rating')->orderBy('reviews_avg_rating','desc');
+    }
 }
