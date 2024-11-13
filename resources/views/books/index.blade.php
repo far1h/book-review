8ec3e8124549911bcadaa35cac4621f26fff3893
far1h <farihmuhammad45@gmail.com>
2024-11-14 00:29:49 +0700

    Styling form to filter books

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,10 +3,10 @@
 @section('content')
     <h1 class="mb-10 text-2xl">Books</h1>
 
-    <form action="{{ route('books.index')}}" method="GET">
-        <input type="text" name="title" placeholder="Search by title" value="{{ request('title')}}" class="input"/>
-        <button type="submit" class="btn">Search</button>
-        <a href="{{ route('books.index')}}">Clear</a>
+    <form action="{{ route('books.index')}}" method="GET" class="mb-4 flex items-center space-x-2">
+        <input type="text" name="title" placeholder="Search by title" value="{{ request('title')}}" class="input h-10"/>
+        <button type="submit" class="btn h-10">Search</button>
+        <a href="{{ route('books.index')}}" class="btn h-10">Clear</a>
     </form>
     <ul>
         @forelse ($books as $book)
