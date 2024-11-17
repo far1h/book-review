106cf15e499d059fad1077c8ca69501489557db9
far1h <farihmuhammad45@gmail.com>
2024-11-17 08:50:07 +0700

    Add cache invalidation for Book

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -77,4 +77,9 @@ class Book extends Model
     {
         return $query->highestRated(now()->subMonths(6), now())->popular(now()->subMonths(6), now())->minReviews(5);
     }
+
+    protected static function booted() {
+        static::updated(fn(Book $book) => cache()->forget('book:'.$book->id));
+        static::deleted(fn(Book $book) => cache()->forget('book:'.$book->id)); 
+    }
 }
